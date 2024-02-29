ad0cda8950e493a9ba36ac409cdbda7a0929ba04
hyer0705 <hyer1205@gmail.com>
2024-02-29 16:53:08 +0900

    add: todo app 전체 레이아웃 구현

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,25 @@
 function Todo() {
-  return <h1>This is Todo Component!</h1>;
+  const todos = [
+    { id: 1, task: "todo app add 기능 구현하기", isCompleted: false },
+    { id: 2, task: "todo app delete 기능 구현하기", isCompleted: false },
+    { id: 3, task: "todo app style 입히기", isCompleted: false },
+  ];
+  return (
+    <main>
+      <form>
+        <input type="text" placeholder="할 일을 입력해주세요." />
+        <button>Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, i) => (
+          <li key={todo.id}>
+            <span>{todo.task}</span>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default Todo;
