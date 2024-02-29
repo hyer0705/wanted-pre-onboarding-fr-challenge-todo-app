4c4e0aec31842711d2f97a935d27eb00f0360da7
hyer0705 <hyer1205@gmail.com>
2024-02-29 18:56:00 +0900

    add: todo input에 onChange 이벤트 구현

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,17 +1,35 @@
+import React, { useState } from "react";
+
 function Todo() {
   const todos = [
     { id: 1, task: "todo app add 기능 구현하기", isCompleted: false },
     { id: 2, task: "todo app delete 기능 구현하기", isCompleted: false },
     { id: 3, task: "todo app style 입히기", isCompleted: false },
   ];
+
+  const [todo, setTodo] = useState("");
+
+  const onTodoChange = (ev: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = ev;
+
+    setTodo(value);
+  };
+
   return (
     <main>
       <form>
-        <input type="text" placeholder="할 일을 입력해주세요." />
+        <input
+          value={todo}
+          onChange={onTodoChange}
+          type="text"
+          placeholder="할 일을 입력해주세요."
+        />
         <button>Add</button>
       </form>
       <ul>
-        {todos.map((todo, i) => (
+        {todos.map((todo) => (
           <li key={todo.id}>
             <span>{todo.task}</span>
             <button>Delete</button>
